31b7f295e80240b3720b48dc02e804bfc528328f
Iyusuf40 <punkgts40@gmail.com>
2022-12-20 20:23:40 +0000

    use async / await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,7 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const photo = await uploadPhoto().catch(null);
-  const user = await createUser().catch(null);
+  const photo = await uploadPhoto().catch(() => null);
+  const user = await createUser().catch(() => null);
+  if (!photo || !user) {
+    return { photo: null, user: null };
+  }
   return { photo, user };
 }
