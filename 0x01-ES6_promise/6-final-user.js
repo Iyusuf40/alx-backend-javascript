cd0413668a695c6580f34424bd0620bb243279aa
Iyusuf40 <punkgts40@gmail.com>
2022-12-21 10:28:02 +0000

    return data as is

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,18 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const res = [];
+  // const res = [];
   return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((data) => {
-      res.push({
-        status: data[0].status,
-        value: data[0].value,
-      });
-      res.push({
-        status: data[1].status,
-        value: data[1].reason,
-      });
-      return res;
-    })
+    .then((data) => data)
   );
 }
