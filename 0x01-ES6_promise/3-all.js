b1c2121aa79ab69b1c288726300f63a260f90247
Iyusuf40 <punkgts40@gmail.com>
2022-12-22 19:07:39 +0000

    create string before consoling

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,9 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((res) => console.log(res[0].body, res[1].firstName, res[1].lastName))
+    .then((res) => {
+      const str = `${res[0].body} ${res[1].firstName} ${res[1].lastName}`;
+      console.log(str);
+    })
     .catch(() => console.log('Signup system offline'));
 }
