6a1b2d358ea4964efe65129c93b23c566d22e0f7
Iyusuf40 <punkgts40@gmail.com>
2022-12-22 15:41:42 +0000

    fix lint

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,32 +1,32 @@
-import Currency from './3-currency';
+// import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
-  };
+  }
 
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
-  };
+  }
 
   set amount(val) {
     this._amount = val;
-  };
+  }
 
   set currency(val) {
     this._currency = val;
-  };
+  }
 
   get amount() {
     return this._amount;
-  };
+  }
 
   get currency() {
     return this._currency;
-  };
+  }
 
   displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-  };
+  }
 }
