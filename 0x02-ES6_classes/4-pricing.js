2d37c0f4219923c957d43ca0df5f094e84b7bc09
Iyusuf40 <punkgts40@gmail.com>
2022-12-22 15:31:26 +0000

    add static method convertPrice

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -4,25 +4,29 @@ export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
-  }
+  };
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  };
 
   set amount(val) {
     this._amount = val;
-  }
+  };
 
   set currency(val) {
     this._currency = val;
-  }
+  };
 
   get amount() {
     return this._amount;
-  }
+  };
 
   get currency() {
     return this._currency;
-  }
+  };
 
   displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-  }
+  };
 }
