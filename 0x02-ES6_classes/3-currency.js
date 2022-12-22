e450a7285359b9bd60dae38b86165cd0cfcb7698
Iyusuf40 <punkgts40@gmail.com>
2022-12-22 14:47:25 +0000

    add cureency class

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,10 @@
+export default class Currency {
+  constructor(code, name) {
+    this._name = name;
+    this._code = code;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
