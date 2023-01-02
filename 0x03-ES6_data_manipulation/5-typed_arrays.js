40f5fb32ba74dd1b0ea5ab76426ae16c84520a98
Iyusuf40 <punkgts40@gmail.com>
2023-01-02 21:46:40 +0000

    use typed arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,6 +3,10 @@ export default function createInt8TypedArray(length, position, value) {
     throw Error('Position outside range');
   }
 
+  if (value > 127 || value < -128) {
+    throw Error('Position outside range');
+  }
+
   const buff = new ArrayBuffer(length);
   const arrInt8view = new Int8Array(buff);
   arrInt8view[position] = value;
