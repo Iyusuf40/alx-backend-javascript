18d00a9b0dd93bfbc191d5d417b29e0391b1d629
Iyusuf40 <punkgts40@gmail.com>
2023-01-02 23:05:58 +0000

    test

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, subStr) {
   const strList = [];
   set.forEach((val) => {
-    if (subStr && val.startsWith(subStr)) {
+    if (subStr && typeof val === 'string' && val.startsWith(subStr)) {
       strList.push(val.slice(subStr.length));
     }
   });
