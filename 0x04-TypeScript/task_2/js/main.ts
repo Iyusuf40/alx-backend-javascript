d91923061a9fe2b4f26e1e7c1f508c04197ae573
Iyusuf40 <punkgts40@gmail.com>
2023-01-04 22:10:07 +0000

    use type literals

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -80,5 +80,15 @@ function executeWork(obj: TeacherInterface | DirectorInterface): string {
 }
 
 
+/*
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
+*/
+
+type Subjects = "Math" | "History"
+
+function teachClass(todayClass: Subjects): string {
+  return todayClass === "Math" ? "Teaching Math" : "Teaching History";
+}
+
+console.log(teachClass('Math'), "<<>>", teachClass('History'));
