d9383c0ee27bad29c79a6ada3065eb99ac394f1f
Iyusuf40 <punkgts40@gmail.com>
2023-02-21 15:11:25 +0000

    add async csv file poarser

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+function res(data) {
+  const content = data.trim();
+  const contentLines = content.split('\n');
+  contentLines.splice(0, 1);
+  const fieldMap = {};
+  for (const item of contentLines) {
+    if (item) {
+      const splittedItem = item.split(',');
+      const field = splittedItem[3];
+      const fn = splittedItem[0];
+      if (fieldMap[field]) {
+        fieldMap[field].push(fn);
+      } else {
+        fieldMap[field] = [];
+        fieldMap[field].push(fn);
+      }
+    }
+  }
+  console.log('Number of students:', contentLines.length);
+  for (const key in fieldMap) {
+    if (key) {
+      const ar = fieldMap[key];
+      console.log(`Number of students in ${key}:`, `${ar.length}.`, `List: ${ar.join(', ')}`);
+    }
+  }
+}
+
+function countStudents(fileName) {
+  // let content;
+
+  return new Promise((resolve) => fs.readFile(fileName, 'utf-8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    // const content = data;
+    res(data);
+    resolve('done');
+  }));
+}
+
+module.exports = countStudents;
