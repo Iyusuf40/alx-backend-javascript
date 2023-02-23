846fb257cc86d934bb79b47fdabc6eb112e4b2a2
Iyusuf40 <punkgts40@gmail.com>
2023-02-23 16:25:14 +0000

    update test to handle decimals

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,10 +1,30 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
+describe('calculateNumber', () => {
+  for (let i = 1; i <= 140; i++) {
+    let rem = i % 10;
+    if (rem) {
+      rem /= 10;
+    } else {
+      rem = 1;
+    }
+    if (rem >= 0.5) {
+      it(`should return ${i + 1}`, () => {
+        assert.equal(calculateNumber(i, rem), i + 1);
+      });
+    } else {
+      it(`should return ${i}`, () => {
+        assert.equal(calculateNumber(i, rem), i);
+      });
+    }
+  }
+});
+
+/* describe('calculateNumber', function () {
   for(let i = 0; i < 140; i++){
     it(`should return ${i * 2}`, function () {
       assert.equal(calculateNumber(i, i), i * 2);
     });
   }
-});
+}); */
